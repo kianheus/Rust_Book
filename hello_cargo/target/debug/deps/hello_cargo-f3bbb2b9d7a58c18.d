4bf0f32b40345d10f4ad70f951cddbc10d1f3ab7
kianheus <Kianheus@gmail.com>
2025-10-16 12:53:22 +0200

    Add code corresponding to ch 1.3

---
+++ hello_cargo/target/debug/deps/hello_cargo-f3bbb2b9d7a58c18.d
@@ -0,0 +1,5 @@
+/home/kian/Documents/GitHub/Rust_Book/hello_cargo/target/debug/deps/hello_cargo-f3bbb2b9d7a58c18.d: src/main.rs
+
+/home/kian/Documents/GitHub/Rust_Book/hello_cargo/target/debug/deps/hello_cargo-f3bbb2b9d7a58c18: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/kian/Documents/GitHub/Rust_Book/hello_cargo/target/debug/hello_cargo: /home/kian/Documents/GitHub/Rust_Book/hello_cargo/src/main.rs
