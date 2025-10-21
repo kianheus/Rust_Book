ab144f627766b9b57bb97989bd6f1cdb0fdf2fe1
kianheus <Kianheus@gmail.com>
2025-10-21 10:30:58 +0200

    Add solution to rustlings exercise vecs2

--- rustlings/exercises/05_vecs/vecs2.rs
+++ rustlings/exercises/05_vecs/vecs2.rs
@@ -4,6 +4,8 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
+
+        output.push(*element * 2);
     }
 
     output
@@ -24,13 +26,15 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     input
         .iter()
         .map(|element| {
-            // ???
+        element * 2    // ???
         })
         .collect()
 }
 
 fn main() {
     // You can optionally experiment here.
+    let output = vec_loop(&[3, 2, 1]);
+    println!("{:?}", output);
 }
 
 #[cfg(test)]
